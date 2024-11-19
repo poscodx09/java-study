04973ac5d1e927784595c329836e5707806c694a
이효원 <ymj07168@gmail.com>
2024-11-19 16:36:59 +0900

    test

--- practice01/src/main/java/prob04/Sol04.java
+++ practice01/src/main/java/prob04/Sol04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Sol04 {
 
 	public static void main(String[] args) {
-		
+
 		
 		Scanner scanner = new Scanner(System.in);
 		String str = scanner.nextLine();
