a95afada9c5b64d88a0883330d37ff03ec34fba2
이효원 <ymj07168@gmail.com>
2024-11-25 16:01:06 +0900

    가변 파라미터

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,22 +1,46 @@
 package prob01;
 
 public class Printer {
-
-	public void println(int i) {
-		System.out.println(i);
+	
+	// 제네릭 지정
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 
-	public void println(boolean b) {
-		System.out.println(b);
+	// 가변 파라미터
+	public int sum(Integer... nums) {
+		int s = 0;
+		for (Integer n : nums) {
+			s += n;
+		}
+		return s;
 	}
+	
+	// 제네릭 + 가변 파라미터
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.print(t + " ");
+		}
+		System.out.print("\n");
+	}
+	
+	
 
-	public void println(double d) {
-		System.out.println(d);
-	}
-
-	public void println(String string) {
-		System.out.println(string);		
-	}
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);		
+//	}
 	
 
 }
