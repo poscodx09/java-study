38093ecf35e173e7b1fa2b6811705f33da78b954
이효원 <ymj07168@gmail.com>
2024-11-25 11:28:33 +0900

    escape

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		
+		System.out.println("c:\\temp");
+
+		// "hello" '"hello"'
+		System.out.println("\"hello\"");
+		
+		// '
+		System.out.println("\'");
+		
+		
+	}
+
+}
