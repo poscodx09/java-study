d45cf38a7f9d7a09534274751cfb556f7db6da03
이효원 <ymj07168@gmail.com>
2024-11-28 10:50:53 +0900

    socket client

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,23 +23,44 @@ public class TCPServer {
 			// 3. accept
 			Socket socket = serverSocket.accept(); // blocking
 			
-			System.out.println("연결 성공");
-			
-			// 4. IO Stream 받아오기
-			InputStream is = socket.getInputStream();
-			
-			// 5. 데이터 읽기
-			byte[] buffer = new byte[256];
-			int readByteCount = is.read(buffer);
-			if (readByteCount == -1) {
-				// closed by client
-				System.out.println("[server] closed by client");
-				return;
+			try {
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+				int remotePort = inetRemoteSocketAddress.getPort();
+				
+				System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+				
+				// 4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				while (true) {
+					// 5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);
+					if (readByteCount == -1) {
+						// closed by client
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "utf-8");
+					System.out.println("[server] received: " + data);
+					
+					// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+				}
+			} catch (IOException e) {
+				System.out.println("error:" + e);
+			} finally {
+				try {
+					if (socket != null && socket.isClosed()) {
+						socket.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			
-			String data = new String(buffer, 0, readByteCount, "utf-8");
-			System.out.println("[server] receive: " + data);
-			
 			
 		} catch (IOException e) {
 			System.out.println("[server] error: + e");
